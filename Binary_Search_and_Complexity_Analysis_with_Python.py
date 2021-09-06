97de7f40457b38177ac270baa24ec58555b538f5
bacha-11 <ijazbacha14@gmail.com>
2021-09-06 15:46:42 +0500

    create multiple test for the problem

--- Binary_Search_and_Complexity_Analysis_with_Python.py
+++ Binary_Search_and_Complexity_Analysis_with_Python.py
@@ -11,14 +11,125 @@ Write a function to help Bob locate the card.
 
 cards =      [13, 12, 11, 7, 4, 3, 2, 1]
 # positions -> 0,  1,  2, 3, 4, 5, 6, 7
-
 query = 7
 
+
+
 # Output
-# position 3
+output = 3
+
+
 
 #function
 def find_card(cards, query):
     pass
 
 
+
+result = find_card(cards, query)
+print(result)
+
+print(result == output)
+
+
+# test = {
+#     "input": {
+#         "cards": [13, 12, 11, 7, 4, 3, 2, 1],
+#         "query": 7
+#     },
+#     "output": 3
+# }
+
+# result = find_card(**test['input']) == test['output']
+# print(result)
+
+tests = []
+
+tests.append({
+     "input": {
+         "cards": [13, 12, 11, 7, 4, 3, 2, 1],
+         "query": 7
+     },
+     "output": 3
+})
+
+
+tests.append({
+    'input': {
+        'cards': [13, 11, 10, 7, 4, 3, 1, 0],
+        'query': 1
+    },
+    'output': 6
+})
+
+# query is the first element
+tests.append({
+    'input': {
+        'cards': [4, 2, 1, -1],
+        'query': 4
+    },
+    'output': 0
+})
+
+
+# query is the last element
+tests.append({
+    'input': {
+        'cards': [3, -1, -9, -127],
+        'query': -127
+    },
+    'output': 3
+})
+
+
+# cards contains just one element, query
+tests.append({
+    'input': {
+        'cards': [6],
+        'query': 6
+    },
+    'output': 0 
+})
+
+
+# cards does not contain query 
+tests.append({
+    'input': {
+        'cards': [9, 7, 5, 2, -9],
+        'query': 4
+    },
+    'output': -1
+})
+
+
+# cards is empty
+tests.append({
+    'input': {
+        'cards': [],
+        'query': 7
+    },
+    'output': -1
+})
+
+
+# numbers can repeat in cards
+tests.append({
+    'input': {
+        'cards': [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
+        'query': 3
+    },
+    'output': 7
+})
+
+
+# query occurs multiple times
+tests.append({
+    'input': {
+        'cards': [8, 8, 6, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
+        'query': 6
+    },
+    'output': 2
+})
+
+
+print(len(tests))

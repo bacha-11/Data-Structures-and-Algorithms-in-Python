31918a9f16835b31f9a040c1030bb648ac9a8e2a
bacha-11 <ijazbacha14@gmail.com>
2021-09-06 14:50:54 +0500

    first we find inputs and output of the problem

---
+++ Binary_Search_and_Complexity_Analysis_with_Python.py
@@ -0,0 +1,24 @@
+'''
+QUESTION 1: Alice has some cards with numbers written on them. 
+She arranges the cards in decreasing order, and lays them out face down in a sequence on a 
+table. She challenges Bob to pick out the card containing a given number by turning over as 
+few cards as possible. 
+Write a function to help Bob locate the card.
+'''
+
+
+# inputs
+
+cards =      [13, 12, 11, 7, 4, 3, 2, 1]
+# positions -> 0,  1,  2, 3, 4, 5, 6, 7
+
+query = 7
+
+# Output
+# position 3
+
+#function
+def find_card(cards, query):
+    pass
+
+
